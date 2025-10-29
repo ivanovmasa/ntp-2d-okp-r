f92f39d47c351847219adf4eaf09e7eee7ebedc7
Masa Ivanov <masa.ivanov04.10@gmail.com>
2025-10-29 18:42:28 +0100

    Add paralellisation

--- main.rs
+++ main.rs
@@ -2,6 +2,10 @@ use macroquad::prelude::*;
 use ::rand::prelude::*;
 use ::rand::rng;
 use ::rand::rngs::ThreadRng;
+use rayon::prelude::*;
+use ::rand::SeedableRng; 
+use ::rand::rngs::SmallRng;
+use std::time::Instant;
 
 #[derive(Clone, Copy, Debug)]
 struct Rect {
@@ -67,6 +71,7 @@ fn roulette_selection(parents: &[Vec<u8>], rng: &mut ThreadRng) -> Vec<(Vec<u8>,
         .collect()
 }
 
+//SERIAL VERSION
 fn two_point_crossover(pairs: &[(Vec<u8>, Vec<u8>)], rng: &mut ThreadRng) -> Vec<Vec<u8>> {
     pairs.iter()
         .flat_map(|(parent1, parent2)| {
@@ -83,6 +88,27 @@ fn two_point_crossover(pairs: &[(Vec<u8>, Vec<u8>)], rng: &mut ThreadRng) -> Vec
         .collect()
 }
 
+//PARALEL VERSION
+// fn two_point_crossover(pairs: &[(Vec<u8>, Vec<u8>)], rng: &mut ThreadRng) -> Vec<Vec<u8>> {
+//     let seed = rng.random::<u64>();
+//     pairs.par_iter()
+//         .enumerate()
+//         .flat_map(|(i, (parent1, parent2))| {
+//             let mut local_rng = SmallRng::seed_from_u64(seed + i as u64);
+//             let len = parent1.len();
+//             let r1 = local_rng.random_range(0..len);
+//             let r2 = local_rng.random_range(0..len);
+//             let (start, end) = if r1 < r2 { (r1, r2) } else { (r2, r1) };
+
+//             let child1 = [&parent1[..start], &parent2[start..end], &parent1[end..]].concat();
+//             let child2 = [&parent2[..start], &parent1[start..end], &parent2[end..]].concat();
+
+//             vec![child1, child2]
+//         })
+//         .collect()
+// }
+
+//SERIAL VERSION
 fn mutation(chromosomes: &Vec<Vec<u8>>, rate: f32, rng: &mut ThreadRng) -> Vec<Vec<u8>> {
     chromosomes.iter().map(|chromosome| {
         let mut mutated = chromosome.clone();
@@ -94,6 +120,24 @@ fn mutation(chromosomes: &Vec<Vec<u8>>, rate: f32, rng: &mut ThreadRng) -> Vec<V
         mutated
     }).collect()
 }
+
+//PARALEL VERSION
+// fn mutation(chromosomes: &Vec<Vec<u8>>, rate: f32, rng: &mut ThreadRng) -> Vec<Vec<u8>> {
+//     let seed = rng.random::<u64>();
+    
+//     chromosomes.par_iter()
+//     .enumerate()
+//     .map(|(i, chromosome)| {
+//         let mut mutated = chromosome.clone();
+//         let mut local_rng = SmallRng::seed_from_u64(seed + i as u64);
+//         for gene in mutated.iter_mut() {
+//             if local_rng.random::<f32>() < rate {
+//                 *gene = if *gene == 0 { 1 } else { 0 };
+//             }
+//         }
+//         mutated
+//     }).collect()
+// }
 
 fn elitism(parents: &[Vec<u8>], children: &[Vec<u8>], rate: f32, population_size: usize)  -> Vec<Vec<u8>>{
     let old_ind_size = (population_size as f32 * rate).round() as usize;
@@ -246,7 +290,7 @@ fn is_contained_in(rect1: &FreeRect, rect2: &FreeRect) -> bool {
         && rect1.y + rect1.height <= rect2.y + rect2.height
 }
 
-
+//SERIAL VERSION
 fn rank_chromosomes(
     population: &[Vec<u8>],
     problem: &Problem,
@@ -263,6 +307,24 @@ fn rank_chromosomes(
     
     ranked
 }
+
+//PARALEL VERSION
+// fn rank_chromosomes(
+//     population: &[Vec<u8>],
+//     problem: &Problem,
+// ) -> Vec<(Vec<u8>, f32)> {
+//     let mut ranked: Vec<(Vec<u8>, f32)> = population
+//         .par_iter()
+//         .map(|chromosome| {
+//             let (_, fitness) = decode_chromosome(chromosome, problem);
+//             (chromosome.clone(), fitness)
+//         })
+//         .collect();
+    
+//     ranked.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
+    
+//     ranked
+// }
 
 fn genetic_algorithm(
     problem: &Problem,
@@ -389,6 +451,8 @@ async fn main() {
     println!("Bin: {}x{}", problem.bin_width, problem.bin_height);
     println!("Rectangles to pack: {}", problem.rectangles.len());
     
+    let total_start = Instant::now();
+
     let (best_chromosome, best_fitness) = genetic_algorithm(
         &problem,
         100,   // population_size
@@ -397,9 +461,19 @@ async fn main() {
         100,   // max_iterations
         &mut rng,
     );
+
+    let ga_duration = total_start.elapsed();
+
+    println!("\n=== PERFORMANCE METRICS ===");
+    println!("GA execution time: {:.3}s ({} ms)", 
+        ga_duration.as_secs_f64(), 
+        ga_duration.as_millis()
+    );
     
     println!("\n=== FINAL RESULT ===");
     println!("Best fitness: {:.2}%", best_fitness * 100.0);
+    println!("Waste percentage: {:.2}%", (1.0 - best_fitness) * 100.0);
+    
     
     let (placed_rects, _) = decode_chromosome(&best_chromosome, &problem);
     println!("Placed {} rectangles:", placed_rects.len());
